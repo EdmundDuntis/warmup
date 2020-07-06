d40bbe302b2db010fa97ae690ead73597456b217
edmund_duntis <edmund_duntis@163.com>
2020-07-06 22:03:27 +0800

    update

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -2,8 +2,9 @@ package test;
 
 public class Main {
 
-	public static void main(String[] args) {
-
-	}
-
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int[] a = new int[]{1, 2};
+        System.out.println(a[0] + a[1]);
+    }
 }
