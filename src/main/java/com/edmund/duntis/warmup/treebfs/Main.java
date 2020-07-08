77fe03f1b79227d4132a5a9767ff4422ebe0d240
edmund_duntis <edmund_duntis@163.com>
2020-07-08 21:57:11 +0800

    update

--- src/main/java/com/edmund/duntis/warmup/treebfs/Main.java
+++ src/main/java/com/edmund/duntis/warmup/treebfs/Main.java
@@ -6,36 +6,55 @@ import java.util.Queue;
 public class Main {
 
 	public static void main(String[] args) {
-		Queue<Node> queue = new LinkedList<Node>();
+		// 构建树
 		Node root = ConstructTree();
 
+		// 简单情况短路操作
 		if (root == null) {
 			return;
 		}
 
-		queue.add(root);
+		// 正常情况下的遍历
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.offer(root); // root首先入队
 
 		while (!queue.isEmpty()) {
-			Node cur = queue.poll();
+			Node cur = queue.poll(); // 出队
 			if (cur != null) {
-				enqueueChildren(cur, queue);
-				process(cur);
+				enqueueChildren(cur, queue); // 子节点入队
+				process(cur); // 处理当前节点
 			}
 		}
 	}
 
+	/**
+	 * 指定节点的子节点全部入队.
+	 * 
+	 * @param cur
+	 * @param q
+	 */
 	private static void enqueueChildren(Node cur, Queue<Node> q) {
 		for (Node c : cur.children) {
-			if (c != null) {
-				q.add(c);
+			if (c != null) { // 非空子节点入队
+				q.offer(c);
 			}
 		}
 	}
 
+	/**
+	 * 处理当前节点.
+	 * 
+	 * @param curNode
+	 */
 	private static void process(Node curNode) {
 		System.out.print(curNode.data + " ");
 	}
 
+	/**
+	 * 构建树.
+	 * 
+	 * @return
+	 */
 	private static Node ConstructTree() {
 		Node root = new Node("A");
 
@@ -50,6 +69,7 @@ public class Main {
 	}
 }
 
+// Node 定义
 class Node {
 	private static int MaxChildCount = 10;
 
@@ -61,7 +81,6 @@ class Node {
 	public Node[] children = new Node[MaxChildCount];
 }
 
-//问题2
 //二叉树层级遍历，Node数据结构为
 //class Node {
 //public String data;
